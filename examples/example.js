c5a63ab7b3ab8e4d92f58066f09ecd902e04870d
mhsjlw <mhsjlw@users.noreply.github.com>
2018-06-01 20:57:21 -0400

    Add more advanced example, add tests

--- examples/example.js
+++ examples/example.js
@@ -3,28 +3,34 @@ const { ProtoDef, Serializer, Parser } = require('../')
 const fs = require('fs')
 const path = require('path')
 
-const protocol = fs.readFileSync(path.join(__dirname, 'simple.pds'), 'utf8')
+const protocol = fs.readFileSync(path.join(__dirname, 'example.pds'), 'utf8')
 
 const proto = new ProtoDef()
 
 proto.addProtocol(protocol)
 
-const parser = new Parser(proto, 'position')
-const serializer = new Serializer(proto, 'position')
+const parser = new Parser(proto, '::example::entity_data')
+const serializer = new Serializer(proto, '::example::entity_data')
 
 serializer.write({
-  x: 1,
-  y: 2,
-  z: 3
+  entity_id: 0,
+  position: {
+    x: 1.0,
+    y: 2.0,
+    z: 3.0
+  },
+  entity_type: {
+    tag: 'player'
+  }
 })
 
-parser.on('error', function (err) {
-  console.log(err.stack)
-  console.log(err.buffer)
+parser.on('error', (err) => {
+  if(err.buffer.length > 0)
+    console.log(err.stack)
 })
 
 serializer.pipe(parser)
 
-parser.on('data', function (chunk) {
+parser.on('data', (chunk) => {
   console.log(JSON.stringify(chunk.data, null, 2))
 })
---
+++ test/simple.test.js
@@ -0,0 +1,37 @@
+const { ProtoDef, Serializer, Parser } = require('../')
+
+const protocol = `def_native("i32");
+
+@export "position"
+def("position") => container {
+    field("x") => ::i32;
+    field("y") => ::i32;
+    field("z") => ::i32;
+};`
+
+describe('', () => {
+  let proto, parser, serializer
+
+  beforeAll(() => {
+    proto = new ProtoDef()
+    proto.addProtocol(protocol)
+    parser = new Parser(proto, 'position')
+    serializer = new Serializer(proto, 'position')
+  })
+
+  test('can serialize and parse', () => {
+    const data = { x: 1, y: 2, z: 3 }
+
+    serializer.write(data)
+
+    parser.on('error', (e) => {
+      throw e
+    })
+
+    serializer.pipe(parser)
+
+    parser.on('data', (chunk) => {
+      expect(chunk.data).toEqual(data)
+    })
+  })
+})
